f147a3f0f5480f60a902e77b2367dd4c3478c574
Don <hello@donbos.co>
2020-03-22 00:04:32 +0800

    simple drawer setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ import Drawer from "./components/drawer";
 function App() {
   return (
     <div className="App">
+        <div>
+          <h1>Blah blah blah</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et scelerisque nisl. Vestibulum sem nibh, rhoncus ut consectetur non, consequat eget metus. Suspendisse quam purus, laoreet nec ligula id, malesuada malesuada purus. Praesent consectetur mi vitae ante sollicitudin, in scelerisque odio laoreet. Curabitur odio erat, auctor nec enim non, ullamcorper consequat nulla. Curabitur luctus mollis nisl, et pulvinar enim sagittis at. Sed diam risus, accumsan sed posuere sit amet, ultricies id velit. Sed scelerisque ex quis imperdiet ultricies. Pellentesque pellentesque purus sit amet fringilla elementum. Aenean pulvinar urna nec elit pharetra, eu bibendum sem malesuada. Sed semper laoreet nunc ut feugiat. Donec dignissim ex pulvinar, volutpat nulla eu, tincidunt metus. Suspendisse sit amet sapien odio. Mauris eu neque dui. Curabitur pharetra lorem sem, eget sollicitudin lorem consequat sed. Quisque faucibus ex eget tellus porta, quis dictum elit dapibus.
+          <p>
+            Integer tempor, augue a pellentesque dapibus, libero magna laoreet tortor, ac efficitur eros urna sed elit. Quisque congue at dui eu dignissim. Duis elit lectus, scelerisque consequat nibh et, bibendum efficitur magna. Ut congue, enim at ornare placerat, quam nisl tincidunt purus, sit amet efficitur lectus ligula et eros. Sed sagittis viverra velit, et sollicitudin sapien sagittis ut. Sed sodales dictum diam, at fermentum ex auctor id. Suspendisse potenti. Maecenas ornare sagittis neque, et varius purus pellentesque eget. Donec consectetur, risus ut ornare vehicula, neque velit pulvinar nulla, eu rhoncus quam massa ac quam. Sed egestas varius mattis. Mauris laoreet mi a augue accumsan sollicitudin. Aenean a libero ac neque gravida tincidunt id non urna. Donec vel tristique lectus, ut consectetur sem.</p>    
+          </p>
+        </div>
         <Drawer />
     </div>
   );
--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -1,10 +1,27 @@
-import React, { Fragment, useState, useContext } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 const Drawer = () => {
+    const [ isOpen, setIsOpen ] = useState(false);
+
+    useEffect(() => {
+        console.log(isOpen);
+    });
+
+    const handleTriggerDrawer = (e:Object) => {
+        setIsOpen(!isOpen);
+    }
     
     return (
         <Fragment>
-            <h1>Hello from Drawer!</h1>
+            <button className="dev-button" onClick={handleTriggerDrawer}>Toggle Drawer</button>
+            {/* Main Wrapper */}
+            <div className={ isOpen ? 'drawer-container open' : 'drawer-container'}>
+                {/* Main Drawer Content */}
+                <div className="drawer-background" onClick={handleTriggerDrawer}></div>
+                <div className="drawer">
+                    <span>Hello from Drawer!</span>
+                </div>
+            </div>
         </Fragment>
     )
 }
