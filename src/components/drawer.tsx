cc771dcc6a086fc9f913632668934d9008c22b8a
Don <hello@donbos.co>
2020-03-21 22:42:53 +0800

    setting up typescript project

---
+++ src/components/drawer.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment, useState, useContext } from 'react';
+
+const Drawer = () => {
+    
+    return (
+        <Fragment>
+            <h1>Hello from Drawer!</h1>
+        </Fragment>
+    )
+}
+
+export default Drawer;
