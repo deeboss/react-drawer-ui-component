ecab508ed7f82fe7db1a46ec4ef18aebc6d49fe7
Don <hello@donbos.co>
2020-03-23 02:01:14 +0800

    touch drag scroll event 1st version

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -4,6 +4,7 @@ import React, { Fragment, useRef, useState, useEffect, useLayoutEffect } from 'r
 const Drawer = () => {
     const [ isOpen, setIsOpen ] = useState(false);
     const [ yCoordinates, setYCoordinates ] = useState({
+        starting: 120,
         current: 120,
         min: 0,
         max: 0,
@@ -12,37 +13,87 @@ const Drawer = () => {
     });
 
 
+    const [ touchYOffset, setTouchYOffset ] = useState({
+        start: 0,
+        delta: 0,
+        smoothing: 0.5
+    });
+
+
     const [ drawerContentHeight, setDrawerContentHeight ] = useState(0);
     
     const drawerEl = useRef<HTMLDivElement>(null);
     const drawerContentEl = useRef<HTMLDivElement>(null);
-
+    
     useEffect(() => {
         // console.log('info:');
-        // console.log(yCoordinates);
-    })
+        if (isOpen) {
+            if (touchYOffset.delta > 0) {
+                // User is touch scrolling up
+                console.log("up");
+                // console.log(yCoordinates.current + touchYOffset.delta);
+                // console.log((yCoordinates.current + touchYOffset.delta) * touchYOffset.smoothing);
+
+
+                if (yCoordinates.current + (touchYOffset.delta * touchYOffset.smoothing) > yCoordinates.max) {
+                    console.log("Lower limit reached");
+                    setYCoordinates({...yCoordinates, current: yCoordinates.max})
+                } else {
+                    setYCoordinates({...yCoordinates, current: yCoordinates.current + (touchYOffset.delta * touchYOffset.smoothing)});
+                }
+
+            } else {
+                // User is touch scrolling down
+                console.log("down");
+                // console.log(yCoordinates.current - touchYOffset.delta);
+                // console.log((yCoordinates.current - touchYOffset.delta) * touchYOffset.smoothing);
+
+
+                if (yCoordinates.current - (touchYOffset.delta * touchYOffset.smoothing) > yCoordinates.minVisible) {
+                    console.log("Upper limit reached");
+                    // hasReachedTop = true;
+                    setYCoordinates({...yCoordinates, current: yCoordinates.minVisible});
+                } else {
+                    setYCoordinates({...yCoordinates, current: yCoordinates.current - (touchYOffset.delta * touchYOffset.smoothing)});
+                }
+            }
+        }
+
+    }, [touchYOffset.delta]);
 
     useLayoutEffect(() => {
         if (null !== drawerEl.current && null !== drawerContentEl.current) {
             setDrawerContentHeight(drawerEl.current.offsetHeight);
             setYCoordinates({
                 ...yCoordinates,
+                current: yCoordinates.starting,
                 windowMax: drawerEl.current.offsetHeight,
                 max: drawerEl.current.offsetHeight - drawerContentEl.current.offsetHeight,
                 minVisible: drawerEl.current.offsetHeight * 0.6
             });
         }
-    }, [])
+    }, [isOpen]);
+
+
+    const handleTouchStart = (e:any) => {
+        setTouchYOffset({...touchYOffset, start: e.touches[0].pageY});
+    }
 
     const handleTouchMove = (e:any) => {
-        console.log(e.touches[0].clientY);
-    }
+
+        setTouchYOffset({...touchYOffset, delta: touchYOffset.start - e.touches[0].pageY});    }
 
 
     const handleScroll = (e:any) => {
+        // let hasReachedTop : boolean = false;
+        // console.log("can this drawer be dismissed by scrolling up?");
+        // console.log(hasReachedTop);
+        // console.log('---------------------------------------------')
 
         if (e.deltaY > 0) {
             // User is scrolling down
+            // hasReachedTop = false;
+            
             if ((yCoordinates.current + e.deltaY) > yCoordinates.max) {
                 console.log("Lower limit reac3hed");
                 setYCoordinates({...yCoordinates, current: yCoordinates.max})
@@ -51,19 +102,18 @@ const Drawer = () => {
             }
         } else {
             // User is scrolling up
-
-
-            
-            if ((yCoordinates.current - e.deltaY) > yCoordinates.minVisible) {
-                console.log("Upper limit reached");
-                setYCoordinates({...yCoordinates, current: yCoordinates.minVisible});
-
+            if (yCoordinates.current === yCoordinates.minVisible) {
+                // && Math.abs(e.deltaY) > yCoordinates.minVisible
                 // If they keep scrolling even further, dismiss the drawer
-                if (Math.abs(e.deltaY) > yCoordinates.minVisible) {
-                    console.log('Dismissing drawer');
-                    setIsOpen(!isOpen);
-                }
+                console.log('Dismissing drawer');
 
+                setIsOpen(!isOpen);
+                // hasReachedTop = false;
+
+            } else if ((yCoordinates.current - e.deltaY) > yCoordinates.minVisible) {
+                console.log("Upper limit reached");
+                // hasReachedTop = true;
+                setYCoordinates({...yCoordinates, current: yCoordinates.minVisible});
             } else {
                 setYCoordinates({...yCoordinates, current: yCoordinates.current - e.deltaY});
             }
@@ -95,6 +145,7 @@ const Drawer = () => {
                     ref={drawerContentEl}
                     style={ { transform: isOpen ? `translateY(${yCoordinates.current}px)` : `translateY(${yCoordinates.windowMax}px)` } }  
                     onMouseDown={handleMouseDown}
+                    onTouchStart={handleTouchStart}
                     onTouchMoveCapture={handleTouchMove}
                     >
                     <span className="drawer-handle-icon"></span>
